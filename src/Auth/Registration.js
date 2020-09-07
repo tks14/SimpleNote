98f4b40fdfb65b46f7cc0578c8d7daf502c78d4d
vm <tks14@yahoo.com>
2020-09-07 13:47:06 -0400

    server post funciontality added, spinner added to be displayed during loading

--- src/Auth/Registration.js
+++ src/Auth/Registration.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from '../axios';
+import Spinner from '../Spinner/Spinner';
 
 class Registration extends Component {
     
@@ -10,24 +12,65 @@ class Registration extends Component {
             email: "",
             password: "",
             password_confirmation: "",
-            registrationErrors: ""
+            registrationErrors: "",
+            loading: false,
         }
     }
 
     handleSubmit(event){
-        
+        const user = {
+            email: this.state.email,
+            password: this.state.password,
+            
+        };
+        axios.post('/users.json', user)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
+
         event.preventDefault();
     }
 
     handleChange(event){
         this.setState({
             [event.target.name]: event.target.value
-        })
+        });
         
     }
 
 
     render(){
+        let  form =         
+            <form onSubmit ={this.handleSubmit}>
+                <input 
+                    type = "email"
+                    name = "email" 
+                    placeholder="Email" 
+                    value = {this.state.email} 
+                    onChange={this.handleChange} 
+                    required 
+                />
+                <input 
+                    type = "password"
+                    name = "password" 
+                    placeholder="Password" 
+                    value = {this.state.password} 
+                    onChange={this.handleChange} 
+                    required 
+                />
+                <input 
+                    type = "password"
+                    name = "password_confirmation" 
+                    placeholder="Password confirmation" 
+                    value = {this.state.password_confirmation} 
+                    onChange={this.handleChange} 
+                    required 
+                />
+                <button>Register</button>
+            </form>;
+
+        if (this.state.loading){
+            form = <Spinner/>
+        }
         return (
             <div>
                 <form onSubmit ={this.handleSubmit}>
---
+++ src/Spinner/Spinner.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const spinner = () => (
+    <div class="loader">Loading...</div>
+);
+
+
+export default spinner;
